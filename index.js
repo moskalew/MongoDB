2e684e64de1a67aa3ded323038b30fc75338cbd2
Andrei <moskalev.web.dev@gmail.com>
2025-05-26 23:10:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const chalk = require('chalk');
+const path = require('path');
+const mongoose = require('mongoose');
+const {
+  addNote,
+  getNotes,
+  removeNote,
+  updateNote,
+} = require('./notes.controller');
+
+const port = 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'pages');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', async (req, res) => {
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false,
+    error: false,
+  });
+});
+
+app.post('/', async (req, res) => {
+  try {
+    await addNote(req.body.title);
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      created: true,
+      error: false,
+    });
+  } catch (e) {
+    console.error('Creation error', e);
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      created: false,
+      error: true,
+    });
+  }
+});
+
+app.delete('/:id', async (req, res) => {
+  await removeNote(req.params.id);
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false,
+    error: false,
+  });
+});
+
+app.put('/:id', async (req, res) => {
+  await updateNote({ id: req.params.id, title: req.body.title });
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false,
+    error: false,
+  });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://xuitos1:Spas1boMongodb@cluster0.3kizskk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(chalk.green(`Server has been started on port ${port}...`));
+    });
+  });
---
+++ notes.controller.js
@@ -0,0 +1,28 @@
+const chalk = require('chalk')
+const Note = require('./models/Note')
+
+async function addNote(title) {
+  await Note.create({ title })
+
+  console.log(chalk.bgGreen('Note was added!'))
+}
+
+async function getNotes() {
+  const notes = await Note.find();
+
+  return notes;
+}
+
+async function removeNote(id) {
+  await Note.deleteOne({ _id: id })
+  console.log(chalk.red(`Note with id="${id}" has been removed.`))
+}
+
+async function updateNote(noteData) {
+  await Note.updateOne({ _id: noteData.id }, { title: noteData.title })
+  console.log(chalk.bgGreen(`Note with id="${noteData.id}" has been updated!`))
+}
+
+module.exports = {
+  addNote, getNotes, removeNote, updateNote
+}
