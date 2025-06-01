a44c0c264b25dd5e6f10e46ce4c1ca8054d2ccd6
Andrei <moskalev.web.dev@gmail.com>
2025-06-01 03:56:22 +0300

    4. Шифрование пароля.

--- users.controller.js
+++ users.controller.js
@@ -1,7 +1,9 @@
 const User = require('./models/User');
+const bcrypt = require('bcrypt');
 
 async function addUser(email, password) {
-  await User.create({ email, password });
+  const passwordHash = await bcrypt.hash(password, 10);
+  await User.create({ email, password: passwordHash });
 }
 
 module.exports = { addUser };
