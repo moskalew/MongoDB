00b30d59a29da0d054a3236f6681396815d012a6
Andrei <moskalev.web.dev@gmail.com>
2025-06-03 22:15:44 +0300

    6. Вход

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const chalk = require('chalk');
 const path = require('path');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const {
   addNote,
   getNotes,
   removeNote,
   updateNote,
 } = require('./notes.controller');
-const { addUser } = require('./users.controller');
+const { addUser, loginUser } = require('./users.controller');
 
 const port = 3000;
 const app = express();
@@ -18,11 +19,34 @@ app.set('views', 'pages');
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+app.get('/login', async (req, res) => {
+  res.render('login', {
+    title: 'Express App',
+    error: undefined,
+  });
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const token = await loginUser(req.body.email, req.body.password);
+
+    res.cookie('token', token);
+
+    res.redirect('/');
+  } catch (e) {
+    res.render('login', {
+      title: 'Express App',
+      error: e.message,
+    });
+  }
+});
 
 app.get('/register', async (req, res) => {
   res.render('register', {
